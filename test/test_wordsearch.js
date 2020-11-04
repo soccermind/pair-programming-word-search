22e27dfb95f5ad1d99c61c59eb636ed19c8b126f
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-04 21:46:03 +0000

    Updated wordsearch.js and its test file (done in pair programming)

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,28 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BIS')
+
+    assert.isTrue(result);
+  });
+
+  it("should should pass by returning false", function() {
+    const result = wordSearch([
+      [],
+    ], 'BIS')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
+    if (letters === []){
+        return false;
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (let i = 0; i < letters.length; i++ ) {
+        let verticalJoin = '';
+        for (let j = 0; j < letters[i].length; j++) {
+            verticalJoin += letters[j][i];
+        }
+        // let horzontalJoin2 = verticalJoin.map(ls => ls.join(''))
+        if (verticalJoin.includes(word)) {
+            return true
+        }
+    }
+    return false;
 }
+
+
 
 module.exports = wordSearch
